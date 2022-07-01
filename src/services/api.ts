5d56da75ab128b295a86ce9c748bfdd82ca821b4
Otávio Morais <otavio.n.morais@hotmail.com>
2022-06-30 23:58:32 -0300

    feat: production api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:3000";
+export const API_URL = "https://truquito-api.otaviomorais.dev";
 
 export const api = axios.create({
   baseURL: API_URL,
